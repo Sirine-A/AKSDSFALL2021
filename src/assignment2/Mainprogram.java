720349e4ef8a354f58955b49c54ae0b85525a894
Sirine-Ak <72799722+Sirine-Ak@users.noreply.github.com>
2020-10-31 23:57:15 +0200

    first commit

---
+++ src/assignment2/Mainprogram.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+/**
+ *
+ * @author hp
+ */
+public class Mainprogram {
+    public static void main(String[] args){
+    MovieDB movieDB = new MovieDB();
+    movieDB.add(new Movie("Conjuring",2019));
+    movieDB.add(new Movie("Imitation Game",2016));
+    movieDB.add(new Movie("Wild Life",2010));
+    movieDB.display();
+    }    
+}
