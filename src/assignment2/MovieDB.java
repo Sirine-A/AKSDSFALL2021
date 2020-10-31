3798904d01df0957d33cda539c5a4a510e412379
Sirine-Ak <72799722+Sirine-Ak@users.noreply.github.com>
2020-10-31 23:57:38 +0200

    first commit

---
+++ src/assignment2/MovieDB.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+/**
+ *
+ * @author hp
+ */
+public class MovieDB {
+    private static final int BUFFER_SIZE=2;
+    int effectiveNbMovies=0;
+Movie[]moviesList=new Movie[BUFFER_SIZE];
+    public Movie[] add(Movie movie){
+    if(moviesList.length==effectiveNbMovies){
+        Movie[]newmoviesList = new Movie[moviesList.length*2];
+   System.arraycopy(moviesList,0,newmoviesList,0,moviesList.length);
+   return newmoviesList;
+}
+    effectiveNbMovies++;
+    moviesList[effectiveNbMovies]=movie;
+    return (null);
+}
+    void display(){
+    for(Movie movie:moviesList){
+        System.out.println("Movie:"+movie.getName()+","+movie.getReleaseyear());
+    }
+}
+
+    
+}
