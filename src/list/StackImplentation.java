5f305dfbe72606594587df0bbe32562db99ae410
Sirine-Ak <72799722+Sirine-Ak@users.noreply.github.com>
2020-11-18 16:33:37 +0200

    4 commit

--- src/list/StackImplentation.java
+++ src/list/StackImplentation.java
@@ -3,12 +3,45 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package list;
+package List;
 
 /**
  *
  * @author hp
  */
 public class StackImplentation {
-    
+    private final list.List st=new ArrayList();
+    public void Stack(){
+        ArrayList Stack = new ArrayList();   
+    }
+    public void push(Object element){
+        st.add(element);
+    }
+    public Object pop(){
+        if(!isEmpty()){
+           st.remove(st.size()-1);
+            Object popValue = null;
+           return popValue;
+        }
+        else{
+      return -1;}  
+    }
+    public Object peek(){
+    return st.remove(st.size()-1);
+    }
+    public boolean isEmpty(){
+      if(st.isEmty()){
+       return true;
+      }
+      else{
+          return false;
+      }
+        
+    }
+    public int size(){
+        return st.size();
+    }
 }
+
+    
+
